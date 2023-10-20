f4093a1e8146e6133ed89a2c2352b8905be606c2
moschetti1 <francisco.moschetti@gmail.com>
2023-10-20 17:19:27 -0300

    Assets dir try catch block

--- _config.js
+++ _config.js
@@ -4,7 +4,12 @@ import { copy } from "https://deno.land/std@0.197.0/fs/copy.ts";
 import { emptyDir } from "https://deno.land/std@0.197.0/fs/empty_dir.ts";
 
 await emptyDir("./assets")
-await copy("../assets", "./assets", {overwrite:true})  
+try {
+    await copy("../assets", "./assets", {overwrite:true})  
+}
+catch {
+    console.log("No assets directory")
+}
 
 const site = lume();
 
